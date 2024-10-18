15bcc065b1bf2c11b5bb1f738e4709ea007eb1b0
Nevo <nevoday2006@gmail.com>
2024-10-19 02:49:50 +0300

    message from the past :O

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/Practice.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/Practice.java
@@ -20,27 +20,36 @@ public class Practice extends LinearOpMode {
 
 //        MMRobot.getInstance().init(OpModeType.NonCompetition.EXPERIMENTING, hardwareMap, gamepad1,gamepad2,telemetry);
 
+
+        /* TODO:
+            hello itay! writing this in 19/10/24 (2:40am)
+            to resolve this issue, try to run the same code without AutoMecanumDrive (like before)
+            if the issue is still there, then there's some other problem in the code, try to comment lines, and see what effects it.
+            if it works, then u know the issue is in AutoMecanumDrive or the implementation of it here.
+            in this case, u could try and debug it, slowly commenting lines and finding the problem.
+            or u could just take the AutoDriveTrain from last year.
+            good luck!
+         */
+
         AutoMecanumDrive drive = new AutoMecanumDrive(hardwareMap);
 
         Pose2d startPose = new Pose2d(-60,0,0);
 
         drive.getDrive().setPoseEstimate(startPose);
 
+        Trajectory START_POINT_TO_BASCET = drive.getDrive().trajectoryBuilder(startPose)
+                .splineToLinearHeading(new Pose2d(-55, 54,Math.toRadians(-60)), Math.toRadians(90))
+                .build();
+
+        Trajectory FROM_THE_FIRST_SCORING_TO_THE_FIRST_INTAKE = drive.getDrive().trajectoryBuilder(START_POINT_TO_BASCET.end())
+                .splineToLinearHeading(new Pose2d(-44,30,Math.toRadians(40)),Math.toRadians(0))
+                .build();
+
         waitForStart();
 
         if (!isStopRequested() && opModeIsActive()) {
 
-            Trajectory START_POINT_TO_BASCET = drive.getDrive().trajectoryBuilder(startPose)
-                    .splineToLinearHeading(new Pose2d(-55, 54,Math.toRadians(-60)), Math.toRadians(90))
-                    .build();
-
             drive.followTrajectory(START_POINT_TO_BASCET);
-
-
-
-            Trajectory FROM_THE_FIRST_SCORING_TO_THE_FIRST_INTAKE = drive.getDrive().trajectoryBuilder(START_POINT_TO_BASCET.end())
-                    .splineToLinearHeading(new Pose2d(-44,30,Math.toRadians(40)),Math.toRadians(0))
-                    .build();
 
             drive.followTrajectory(FROM_THE_FIRST_SCORING_TO_THE_FIRST_INTAKE);
 
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SubSystems/AutoMecanumDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SubSystems/AutoMecanumDrive.java
@@ -8,15 +8,15 @@ import org.firstinspires.ftc.teamcode.Libraries.RoadRunner.drive.SampleMecanumDr
 import org.firstinspires.ftc.teamcode.Libraries.RoadRunner.trajectorysequence.TrajectorySequence;
 import org.firstinspires.ftc.teamcode.MMRobot;
 
-public class AutoMecanumDrive extends SubsystemBase{
+public class AutoMecanumDrive extends SubsystemBase {
 
     private final SampleMecanumDrive drive;
 
-    public AutoMecanumDrive(HardwareMap hardwareMap){
+    public AutoMecanumDrive(HardwareMap hardwareMap) {
         drive = new SampleMecanumDrive(hardwareMap);
     }
 
-    public SampleMecanumDrive getDrive(){
+    public SampleMecanumDrive getDrive() {
         return drive;
     }
 
