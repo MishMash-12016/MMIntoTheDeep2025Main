752256d82f683c78dff41c2624ebde8c51874bad
boozmcooz <selaayden@gmail.com>
2025-01-08 17:11:23 +0200

    intake sample

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CommandGroup/IntakeSampleCommand.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CommandGroup/IntakeSampleCommand.java
@@ -3,6 +3,7 @@ package org.firstinspires.ftc.teamcode.CommandGroup;
 import com.arcrobotics.ftclib.command.Command;
 import com.arcrobotics.ftclib.command.ParallelCommandGroup;
 import com.arcrobotics.ftclib.command.SequentialCommandGroup;
+import com.arcrobotics.ftclib.command.WaitCommand;
 
 import org.firstinspires.ftc.teamcode.MMRobot;
 import org.firstinspires.ftc.teamcode.SubSystems.IntakeArm;
@@ -13,17 +14,18 @@ public class IntakeSampleCommand {
 
     public static Command prepareSampleIntake(double trigger){
         return new SequentialCommandGroup(
-                MMRobot.getInstance().mmSystems.intakEndUnit.openIntakeClaw(),
                 MMRobot.getInstance().mmSystems.linearIntake.setPosition(trigger),
-                MMRobot.getInstance().mmSystems.intakeArm.setPosition(IntakeArm.intakePose)
+                MMRobot.getInstance().mmSystems.intakeArm.setPosition(IntakeArm.prepareSampleIntake),
+                MMRobot.getInstance().mmSystems.intakEndUnit.openIntakeClaw()
         );
-
-    }    public static Command startSampleCollection(){
+    }
+    public static Command SampleIntake(){
         return new SequentialCommandGroup(
                 MMRobot.getInstance().mmSystems.intakeArm.setPosition(IntakeArm.intakePose), //collect pose
                 MMRobot.getInstance().mmSystems.intakEndUnit.closeIntakeClaw(),
+                new WaitCommand(200),
                 MMRobot.getInstance().mmSystems.intakeArm.setPosition(IntakeArm.transferPose),
-                MMRobot.getInstance().mmSystems.scoringArm.setPosition(ScoringArm.intakeSpecimen),
+                MMRobot.getInstance().mmSystems.scoringArm.setPosition(ScoringArm.transferHold),
                 MMRobot.getInstance().mmSystems.linearIntake.setPosition(LinearIntake.closedPose),
                 MMRobot.getInstance().mmSystems.scoringClawEndUnit.closeScoringClaw()
         );
