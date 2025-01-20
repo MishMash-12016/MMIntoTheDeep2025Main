95467db94f5a0b6acd4dfe2992f917fd75eb476a
boozmcooz <selaayden@gmail.com>
2025-01-20 10:31:04 +0200

    working on red far

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/RedFar.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/RedFar.java
@@ -21,10 +21,13 @@ public class RedFar {
                 .splineToLinearHeading(new Pose2d(5, -30, Math.toRadians(90)), Math.toRadians(90)) //score secimen
                 .setTangent(Math.toRadians(360))//240))
                 .splineToLinearHeading(new Pose2d(48.84, -44.84, Math.toRadians(270)), Math.toRadians(0)) //pick up 1
-                .setTangent(Math.toRadians(0))
-                .splineToLinearHeading(new Pose2d(57.875, -44.84, Math.toRadians(270)), Math.toRadians(0)) //pick up 2
-                .setTangent(Math.toRadians(190))
-                .splineToLinearHeading(new Pose2d(50.8, -45.8, Math.toRadians(315)), Math.toRadians(190)) //pick up 3
+                .turn(Math.toRadians(180))
+                .setTangent(Math.toRadians(180))
+                        .splineToLinearHeading(new Pose2d(5, -30, Math.toRadians(90)), Math.toRadians(90)) //score
+//                .setTangent(Math.toRadians(0))
+//                .splineToLinearHeading(new Pose2d(57.875, -44.84, Math.toRadians(270)), Math.toRadians(0)) //pick up 2
+//                .setTangent(Math.toRadians(190))
+//                .splineToLinearHeading(new Pose2d(50.8, -45.8, Math.toRadians(315)), Math.toRadians(190)) //pick up 3
                 .build());
 
 
