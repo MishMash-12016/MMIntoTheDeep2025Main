e40f21ee4810465ae81ce7f1bd17a75c4b123a5c
drorlifshitz-12016 <mishmashbgu@gmail.com>
2025-01-31 14:55:00 +0200

    working on auto with linear intake

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/RedFar.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/RedFar.java
@@ -1,52 +1,39 @@
 package com.example.meepmeeptesting;
 
 
+import com.acmerobotics.roadrunner.drive.MecanumDrive;
 import com.acmerobotics.roadrunner.geometry.Pose2d;
 import com.acmerobotics.roadrunner.geometry.Vector2d;
+import com.acmerobotics.roadrunner.trajectory.constraints.ProfileAccelerationConstraint;
+import com.acmerobotics.roadrunner.trajectory.constraints.TranslationalVelocityConstraint;
 
 import org.rowlandhall.meepmeep.MeepMeep;
 import org.rowlandhall.meepmeep.roadrunner.DefaultBotBuilder;
 import org.rowlandhall.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
 
+
 public class RedFar {
     public static void main(String[] args) {
         MeepMeep meepMeep = new MeepMeep(600);
+
+         double maxWheelVel = 70;
+        double maxProfileAccel = 70;
 
         RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
                 .setDimensions(11.02,14.5)
                 // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
                 .setConstraints(100, 100, Math.toRadians(180), Math.toRadians(180), 15)
 
-                .followTrajectorySequence(drive-> drive.trajectorySequenceBuilder (new Pose2d(5.5, -65.5, Math.toRadians(90.00)))
+                .followTrajectorySequence(drive-> drive.trajectorySequenceBuilder (new Pose2d(5.5, -65.5, Math.toRadians(90)))
                         .setTangent(Math.toRadians(90))
-                        .splineToSplineHeading(new Pose2d(-5, -27, Math.toRadians(90)), Math.toRadians(90)) //preload
-                        .setTangent(Math.toRadians(270))
-                        .splineToSplineHeading(new Pose2d(-5,-45, Math.toRadians(90)), Math.toRadians(270))
+                        .splineToConstantHeading(new Vector2d(-5, -27), Math.toRadians(90)) //preload
+                        .splineToConstantHeading(new Vector2d(-5,-40), Math.toRadians(270))
 
-                        .setTangent(Math.toRadians(0))
-                        .splineToSplineHeading(new Pose2d(38, -44.84, Math.toRadians(90)), Math.toRadians(0))
-                        .setTangent(Math.toRadians(90))
-                        .splineToSplineHeading(new Pose2d(38,-14, Math.toRadians(90)), Math.toRadians(90))
-                        .setTangent(Math.toRadians(0))
-                        .splineToSplineHeading(new Pose2d(46,-14 , Math.toRadians(90)), Math.toRadians(0))
-                        .setTangent(Math.toRadians(270))
-                        .splineToSplineHeading(new Pose2d(46,-57,Math.toRadians(90)), Math.toRadians(270))
-                        .setTangent(Math.toRadians(90))
-                       .splineToSplineHeading(new Pose2d(46,-14,Math.toRadians(90)), Math.toRadians(90))
-                        .setTangent(Math.toRadians(0))
-                        .splineToSplineHeading(new Pose2d(55,-14 ,Math.toRadians(90)), Math.toRadians(0))
-                        .setTangent(Math.toRadians(270))
-                        .splineToSplineHeading(new Pose2d(55,-57 ,Math.toRadians(90)), Math.toRadians(270))
-                        .setTangent(Math.toRadians(90))
-                        .splineToSplineHeading(new Pose2d(55,-14  ,Math.toRadians(90)), Math.toRadians(90))
-                        .setTangent(Math.toRadians(0))
-                        .splineToSplineHeading(new Pose2d(64,-14  ,Math.toRadians(90)), Math.toRadians(0))
-                        .setTangent(Math.toRadians(270))  //pick up first
-                        .splineToSplineHeading(new Pose2d(64, -67, Math.toRadians(90)), Math.toRadians(270))
-//
-                        .setTangent(160)
-                        .splineToSplineHeading(new Pose2d(-5, -28, Math.toRadians(90)), Math.toRadians(90))
-                        .splineToSplineHeading(new Pose2d(-5, -45, Math.toRadians(90)), Math.toRadians(90))
+                        .splineToSplineHeading(new Pose2d(40,-40,Math.toRadians(270)), Math.toRadians(0))
+                        .turn(Math.toRadians(-180))
+
+
+
 
                         .build());
 
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Libraries/RoadRunner/tuning/LocalizationTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Libraries/RoadRunner/tuning/LocalizationTest.java
@@ -7,11 +7,13 @@ import com.acmerobotics.roadrunner.Pose2d;
 import com.acmerobotics.roadrunner.PoseVelocity2d;
 import com.acmerobotics.roadrunner.Vector2d;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.firstinspires.ftc.teamcode.*;
 import org.firstinspires.ftc.teamcode.Libraries.RoadRunner.Drawing;
 import org.firstinspires.ftc.teamcode.Libraries.RoadRunner.PinpointDrive;
 
+@TeleOp
 public class LocalizationTest extends LinearOpMode {
     @Override
     public void runOpMode() throws InterruptedException {
