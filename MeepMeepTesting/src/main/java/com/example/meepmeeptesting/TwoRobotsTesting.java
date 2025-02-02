846d3a12c39eacd7f5e9bc97d4a2b9c7d080199f
drorlifshitz-12016 <mishmashbgu@gmail.com>
2025-02-02 23:22:54 +0200

    2 samples into human player zone pushed using intake

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/TwoRobotsTesting.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/TwoRobotsTesting.java
@@ -1,72 +1,148 @@
 package com.example.meepmeeptesting;
 
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.acmerobotics.roadrunner.geometry.Vector2d;
 
 import org.rowlandhall.meepmeep.MeepMeep;
-import org.rowlandhall.meepmeep.core.colorscheme.scheme.ColorSchemeBlueLight;
 import org.rowlandhall.meepmeep.roadrunner.DefaultBotBuilder;
 import org.rowlandhall.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
 
 public class TwoRobotsTesting {
+
+    //collection from human player
+    private static final Pose2d collectionSpecimanPos = new Pose2d(45,-67,Math.toRadians(90));
+
+
+    //scoring positions
+    private static final Vector2d scoreSpecimanPos = new Vector2d(5.5, -27); // when driving to scoring location
+    private static final Vector2d scoreSpecimanPos2 = new Vector2d(5.5, -40); // when driving backwards to score
+    private static final int scoreSpecimanXConst = 3; // the spacing of the scored specimens
+
+
+    //pushing to human player
+    private static final Pose2d pushSamplePos = new Pose2d(33.24,-38, Math.toRadians(230));
+    private static final float pushingSampleXConst = 10; // the spacing between the samples
+    /*
+     * a adjustive const for each sample might be needed
+     */
+
+
+    //parking position
+    private static final Pose2d parkPos = new Pose2d(45, -60, Math.toRadians(90));
+
+
     public static void main(String[] args) {
-//        MeepMeep meepMeep = new MeepMeep(600);
-//
-//        RoadRunnerBotEntity myBlueBot = new DefaultBotBuilder(meepMeep)
-//                .setDimensions(11.02,14.5)
-//                // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
-//                .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
-//                .setColorScheme(new ColorSchemeBlueLight())
-//                .build();
-//
-//        myBlueBot.runAction(myBlueBot.getDrive().actionBuilder (new Pose2d(5.5, 65.5, Math.toRadians(90.00)))
-//                .setTangent(Math.toRadians(270))
-//                .splineToLinearHeading(new Pose2d(5, 35, Math.toRadians(90)), Math.toRadians(90)) //score secimen
-//                .setTangent(Math.toRadians(0))
-//                .splineToLinearHeading(new Pose2d(48.84, 40.84, Math.toRadians(270)), Math.toRadians(0)) //pick up 1
-//                .setTangent(Math.toRadians(60))
-//                .splineToLinearHeading(new Pose2d(55.97, 59.18, Math.toRadians(225)), Math.toRadians(60)) //go back
-//                .setTangent(Math.toRadians(275))
-//                .splineToLinearHeading(new Pose2d(57.875, 44.84, Math.toRadians(270)), Math.toRadians(270)) //pick up 2
-//                .setTangent(Math.toRadians(275))
-//                .splineToLinearHeading(new Pose2d(55.97, 54.18, Math.toRadians(225)), Math.toRadians(270)) //go back
-//                .setTangent(Math.toRadians(60))//270))
-//                .splineToLinearHeading(new Pose2d(50.8, 45.8, Math.toRadians(315)), Math.toRadians(260)) //pick up 3
-//                .setTangent(Math.toRadians(60))
-//                .splineToLinearHeading(new Pose2d(55.97, 59.18, Math.toRadians(225)), Math.toRadians(60)) //go back
-//
-//                .build());
-//
-//        RoadRunnerBotEntity myRedBot = new DefaultBotBuilder(meepMeep)
-//                .setDimensions(11.02,14.5)
-//                // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
-//                .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
-//                .setColorScheme(new ColorSchemeRedDark())
-//                .build();
-//
-//        myRedBot.runAction(myRedBot.getDrive().actionBuilder (new Pose2d(-5.5, -65.5, Math.toRadians(90.00)))
-//                        .setTangent(Math.toRadians(100))
-//                        .splineToLinearHeading(new Pose2d(-5, -30, Math.toRadians(90)), Math.toRadians(90)) //score secimen
-//                        .setTangent(Math.toRadians(240))
-//                        .splineToLinearHeading(new Pose2d(-48.84, -44.84, Math.toRadians(270)), Math.toRadians(180)) //pick up 1
-//                        .setTangent(Math.toRadians(240))
-//                        .splineToLinearHeading(new Pose2d(-55.97, -59.18, Math.toRadians(225)), Math.toRadians(270)) //go back
-//                        .setTangent(Math.toRadians(100))
-//                        .splineToLinearHeading(new Pose2d(-57.875, -44.84, Math.toRadians(270)), Math.toRadians(95)) //pick up 2
-//                        .setTangent(Math.toRadians(275))
-//                        .splineToLinearHeading(new Pose2d(-55.97, -59.18, Math.toRadians(225)), Math.toRadians(270)) //go back
-//                        .setTangent(Math.toRadians(100))
-//                        .splineToLinearHeading(new Pose2d(-50.8, -45.8, Math.toRadians(315)), Math.toRadians(95)) //pick up 3
-//                        .setTangent(Math.toRadians(270))
-//                        .splineToLinearHeading(new Pose2d(-55.97, -59.18, Math.toRadians(225)), Math.toRadians(270)) //go back
-//
-//                .build());
+        MeepMeep meepMeep = new MeepMeep(800);
+
+        RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep).setDimensions(11.02,14.5)
+                // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
+                .setConstraints(100, 100, Math.toRadians(720), Math.toRadians(720), 15)
+                .followTrajectorySequence(drive -> drive.trajectorySequenceBuilder(new Pose2d(5.5, -65.5, Math.toRadians(90.00)))
+
+                        //score 1
+                        .splineToConstantHeading(scoreSpecimanPos,Math.toRadians(90))
+                        .splineToConstantHeading(scoreSpecimanPos2,Math.toRadians(270))
+
+//                        //push 1
+                        .setTangent(Math.toRadians(0))
+                        .splineToSplineHeading(pushSamplePos, Math.toRadians(0))
+                        .addTemporalMarkerOffset(1, () -> {
+                            // I don't have access to MMRobot, just add it to the imports and write the commands
+                            //open linear - max opening
+                            //intake arm - might need a new position
+                            //intake rotator - turn 90 degrees
+
+                            //you might need to close the liner intake after each "push"
+                        })
+                        .setTangent(Math.toRadians(300))
+                        .splineToLinearHeading(new Pose2d(32.24,-53, Math.toRadians(120)),Math.toRadians(240))
+
+                        .setTangent(Math.toRadians(80))
+                        .splineToLinearHeading(new Pose2d(33.24+6,-38,Math.toRadians(230)),Math.toRadians(70))
+                        .setTangent(Math.toRadians(300))
+                        .splineToLinearHeading(new Pose2d(32.25+6,-53, Math.toRadians(120)),Math.toRadians(240))
+//                        //push 2
+//                        .splineToLinearHeading(pushSamplePos.plus(new Pose2d(pushingSampleXConst,0,0)),Math.toRadians(180))
+//                        .addTemporalMarkerOffset(1, () -> {
 //
 //
-//        meepMeep.setBackground(MeepMeep.Background.FIELD_INTO_THE_DEEP_JUICE_DARK)
+//                        })
+//                        .turn(Math.toRadians(-90))
+//                        //push 3
+//                        .splineToLinearHeading(pushSamplePos.plus(new Pose2d(pushingSampleXConst*2,0,0)),Math.toRadians(180))
+//                        .addTemporalMarkerOffset(1, () -> {
 //
-//                .setDarkMode(true)
-//                .setBackgroundAlpha(0.95f)
-//                .addEntity(myRedBot)
-//                .addEntity(myBlueBot)
-//                .start();
+//
+//                        })
+//                        .turn(Math.toRadians(-70))
+//
+//
+//
+//                        //score 2
+//                        .addTemporalMarkerOffset(0, () -> {
+//                            // I don't have access to MMRobot, just add it to the imports and write the commands
+//                            //for prepare speciman collection
+//                        })
+//                        .splineToLinearHeading(collectionSpecimanPos,Math.toRadians(250)) //a change here might be needed, change angle to 270
+//                        .addTemporalMarkerOffset(0, () -> {
+//                            // I don't have access to MMRobot, just add it to the imports and write the commands
+//                            //for speciman collection
+//                        })
+//                        .splineToConstantHeading(scoreSpecimanPos.minus(new Vector2d(scoreSpecimanXConst,0)), Math.toRadians(90))
+//                        .splineToConstantHeading(scoreSpecimanPos2.minus(new Vector2d(scoreSpecimanXConst,0)), Math.toRadians(90))
+//
+//                        //score 3
+//                        .addTemporalMarkerOffset(0, () -> {
+//                            // I don't have access to MMRobot, just add it to the imports and write the commands
+//                            //for prepare speciman collection
+//                        })
+//                        .splineToLinearHeading(collectionSpecimanPos,Math.toRadians(270))
+//                        .waitSeconds(.000001) //this time delay is for adjusting the path without splitting it in to two paths
+//                        .addTemporalMarkerOffset(0, () -> {
+//                            // I don't have access to MMRobot, just add it to the imports and write the commands
+//                            //for speciman collection
+//                        })
+//                        .splineToConstantHeading(scoreSpecimanPos.minus(new Vector2d(scoreSpecimanXConst*2,0)), Math.toRadians(90))
+//                        .splineToConstantHeading(scoreSpecimanPos2.minus(new Vector2d(scoreSpecimanXConst*2,0)),Math.toRadians(90))
+//
+//                        //score 4
+//                        .addTemporalMarkerOffset(0, () -> {
+//                            // I don't have access to MMRobot, just add it to the imports and write the commands
+//                            //for prepare speciman collection
+//                        })
+//                        .splineToLinearHeading(collectionSpecimanPos,Math.toRadians(270))
+//                        .waitSeconds(.000001)
+//                        .addTemporalMarkerOffset(0, () -> {
+//                            // I don't have access to MMRobot, just add it to the imports and write the commands
+//                            //for speciman collection
+//                        })
+//                        .splineToConstantHeading(scoreSpecimanPos.minus(new Vector2d(scoreSpecimanXConst*3,0)), Math.toRadians(90))
+//                        .splineToConstantHeading(scoreSpecimanPos2.minus(new Vector2d(scoreSpecimanXConst*3,0)),Math.toRadians(90))
+//
+//                        //score 5
+//                        .addTemporalMarkerOffset(0, () -> {
+//                            // I don't have access to MMRobot, just add it to the imports and write the commands
+//                            //for prepare speciman collection
+//                        })
+//                        .splineToLinearHeading(collectionSpecimanPos,Math.toRadians(270))
+//                        .waitSeconds(.000001)
+//                        .addTemporalMarkerOffset(0, () -> {
+//                            // I don't have access to MMRobot, just add it to the imports and write the commands
+//                            //for speciman collection
+//                        })
+//                        .splineToConstantHeading(scoreSpecimanPos.minus(new Vector2d(scoreSpecimanXConst*4,0)), Math.toRadians(90))
+//                        .strafeTo(scoreSpecimanPos2.minus(new Vector2d(scoreSpecimanXConst*4)))
+//
+//                        //park
+//                        .splineToLinearHeading(parkPos, Math.toRadians(0))
+
+                        .build());
+
+
+        meepMeep.setBackground(MeepMeep.Background.FIELD_INTOTHEDEEP_JUICE_DARK)
+                .setDarkMode(true)
+                .setBackgroundAlpha(0.95f)
+                .addEntity(myBot)
+                .start();
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/TestAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/TestAuto.java
@@ -3,14 +3,22 @@ package org.firstinspires.ftc.teamcode.Autonomous;
 import com.acmerobotics.dashboard.FtcDashboard;
 import com.acmerobotics.roadrunner.Pose2d;
 import com.acmerobotics.roadrunner.TrajectoryActionBuilder;
+import com.acmerobotics.roadrunner.TranslationalVelConstraint;
+import com.acmerobotics.roadrunner.TurnConstraints;
+import com.acmerobotics.roadrunner.Twist2d;
 import com.acmerobotics.roadrunner.Vector2d;
+import com.arcrobotics.ftclib.command.ParallelCommandGroup;
 import com.arcrobotics.ftclib.command.SequentialCommandGroup;
+import com.arcrobotics.ftclib.command.WaitCommand;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 
 import org.firstinspires.ftc.teamcode.CommandGroup.ScoringSpecimanCommand;
 import org.firstinspires.ftc.teamcode.Libraries.MMLib.MMOpMode;
+import org.firstinspires.ftc.teamcode.Libraries.RoadRunner.MecanumDrive;
 import org.firstinspires.ftc.teamcode.Libraries.RoadRunner.PinpointDrive;
 import org.firstinspires.ftc.teamcode.MMRobot;
+import org.firstinspires.ftc.teamcode.SubSystems.IntakeArm;
+import org.firstinspires.ftc.teamcode.SubSystems.IntakeEndUnitRotator;
 import org.firstinspires.ftc.teamcode.SubSystems.LinearIntake;
 import org.firstinspires.ftc.teamcode.utils.OpModeType;
 
@@ -34,25 +42,52 @@ public class TestAuto extends MMOpMode {
         MMRobot.getInstance().mmSystems.scoringClawEndUnit.closeScoringClaw();// pre load
         MMRobot.getInstance().mmSystems.linearIntake.setPosition(0);
 
-        TrajectoryActionBuilder driveToScorePreloadSpecimen = drive.actionBuilder(currentPose)
-                .setTangent(Math.toRadians(90))
-                .splineToConstantHeading(new Vector2d(-5, -27), Math.toRadians(90)); //preload
-        TrajectoryActionBuilder driveToScorePreloadSpecimen2 = driveToScorePreloadSpecimen.endTrajectory().fresh()
-                .splineToConstantHeading(new Vector2d(-5,-40), Math.toRadians(270));
-        TrajectoryActionBuilder driveToPushSample1 = driveToScorePreloadSpecimen2.endTrajectory().fresh()
-                .splineToSplineHeading(new Pose2d(40,-40,Math.toRadians(270)), Math.toRadians(0));
-        TrajectoryActionBuilder turn = driveToPushSample1.endTrajectory().fresh()
-                .turn(Math.toRadians(-180));
+        final Vector2d scoreSpecimenPose = new Vector2d(5.5, -27); // when driving to scoring location
+        final Vector2d scoreSpecimenPos2 = new Vector2d(5.5, -40); // when driving backwards to score
+        final Pose2d pushSamplePos = new Pose2d(32.8,-38, Math.toRadians(230));
+        final double pushingSampleXConst= 10;
+        final double halfOpenClaw =0.68;
+        final double rotator =0.65;
 
+        TrajectoryActionBuilder driveToScorePreloadSpecimen = drive.actionBuilder(currentPose)
+                .splineToConstantHeading(scoreSpecimenPose,Math.toRadians(90));
+        TrajectoryActionBuilder driveToScorePreloadSpecimen2 = driveToScorePreloadSpecimen.endTrajectory().fresh()
+                .splineToConstantHeading(scoreSpecimenPos2,Math.toRadians(270)); //preload
+        TrajectoryActionBuilder driveToPush1 = driveToScorePreloadSpecimen2.endTrajectory().fresh()
+                .setTangent(Math.toRadians(0))
+                .splineToSplineHeading(pushSamplePos, Math.toRadians(0));
+         TrajectoryActionBuilder turnRobot =driveToPush1.endTrajectory().fresh()
+                 .setTangent(Math.toRadians(300))
+                 .splineToLinearHeading(new Pose2d(32.8,-53, Math.toRadians(120)),Math.toRadians(240),new TranslationalVelConstraint(MecanumDrive.PARAMS.maxWheelVel*0.5));
+        TrajectoryActionBuilder driveToPush2 = turnRobot.endTrajectory().fresh()
+                .setTangent(Math.toRadians(80))
+                .splineToLinearHeading(new Pose2d(38.8,-38,Math.toRadians(230)),Math.toRadians(70));
+
+    TrajectoryActionBuilder turnRobot2 =driveToPush2.endTrajectory().fresh()
+            .setTangent(Math.toRadians(300))
+            .splineToLinearHeading(new Pose2d(38.8,-53, Math.toRadians(120)),Math.toRadians(240),new TranslationalVelConstraint(MecanumDrive.PARAMS.maxWheelVel*0.5));
         new SequentialCommandGroup(
                 new ActionCommand(driveToScorePreloadSpecimen.build()).alongWith(
-                        ScoringSpecimanCommand.SpecimanScore()
-                ),
+                        ScoringSpecimanCommand.SpecimanScore()),
                 new ActionCommand(driveToScorePreloadSpecimen2.build()),
-                new ActionCommand(driveToPushSample1.build()).alongWith(
-                        robotInstance.mmSystems.linearIntake.setPosition(LinearIntake.maxOpening)
-                ),
-                new ActionCommand(turn.build())
+                new ActionCommand(driveToPush1.build()).alongWith(
+                    new ParallelCommandGroup(
+                    robotInstance.mmSystems.linearIntake.setPosition(LinearIntake.maxOpening),
+                            robotInstance.mmSystems.intakeArm.setPosition(IntakeArm.IntakeArmState.PREPARE_SAMPLE_INTAKE),
+                            robotInstance.mmSystems.intakeEndUnitRotator.setPosition(rotator),
+                            robotInstance.mmSystems.intakEndUnit.setPose(halfOpenClaw))),
+                robotInstance.mmSystems.intakeArm.setPosition(0.62),
+                new WaitCommand(500),
+                new ActionCommand(turnRobot.build()),
+                new ActionCommand(driveToPush2.build()).alongWith(
+                        new ParallelCommandGroup(
+                                robotInstance.mmSystems.linearIntake.setPosition(LinearIntake.maxOpening),
+                                robotInstance.mmSystems.intakeArm.setPosition(IntakeArm.IntakeArmState.PREPARE_SAMPLE_INTAKE),
+                                robotInstance.mmSystems.intakeEndUnitRotator.setPosition(rotator),
+                                robotInstance.mmSystems.intakEndUnit.setPose(halfOpenClaw))),
+                robotInstance.mmSystems.intakeArm.setPosition(0.62),
+                new WaitCommand(800),
+                new ActionCommand(turnRobot2.build())
 
  ).schedule();
     }
