001458157641bc3b4eb1455a5e0bdb28aad45b01
eran piestun <epiestun@gmail.com>
2025-03-12 16:38:16 +0200

    Limelight commands

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CommandGroup/IntakeSampleCommand.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CommandGroup/IntakeSampleCommand.java
@@ -1,14 +1,18 @@
 package org.firstinspires.ftc.teamcode.CommandGroup;
 
 import com.arcrobotics.ftclib.command.Command;
+import com.arcrobotics.ftclib.command.InstantCommand;
 import com.arcrobotics.ftclib.command.ParallelCommandGroup;
 import com.arcrobotics.ftclib.command.SequentialCommandGroup;
 import com.arcrobotics.ftclib.command.WaitCommand;
+import com.qualcomm.robotcore.hardware.HardwareMap;
 
+import org.firstinspires.ftc.teamcode.CommandGroup.limelight.limelightGetter;
+import org.firstinspires.ftc.teamcode.Libraries.RoadRunner.PinpointDrive;
 import org.firstinspires.ftc.teamcode.MMRobot;
+import org.firstinspires.ftc.teamcode.SubSystems.IntakeArm;
 import org.firstinspires.ftc.teamcode.SubSystems.IntakeArm.IntakeArmState;
 import org.firstinspires.ftc.teamcode.SubSystems.IntakeEndUnitRotator;
-import org.firstinspires.ftc.teamcode.SubSystems.LinearIntake;
 import org.firstinspires.ftc.teamcode.SubSystems.LinearIntake.LinearIntakeState;
 import org.firstinspires.ftc.teamcode.SubSystems.ScoringArm.ScoringArmState;
 import org.firstinspires.ftc.teamcode.SubSystems.ScoringEndUnitElbow;
@@ -45,6 +49,30 @@ public class IntakeSampleCommand {
                 MMRobot.getInstance().mmSystems.linearIntake.setPosition(LinearIntakeState.CLOSED_POSE)
 
 
+        );
+    }
+
+    public static Command limeLightIntake_TeleOp(HardwareMap hardwareMap){
+        return new SequentialCommandGroup(
+                MMRobot.getInstance().mmSystems.intakeArm.setPosition(IntakeArm.IntakeArmState.PREPARE_SAMPLE_INTAKE),
+                MMRobot.getInstance().mmSystems.intakEndUnit.openIntakeClaw(),
+                MMRobot.getInstance().mmSystems.scoringEndUnitElbow.setPosition(ScoringEndUnitElbow.ScoringElbowState.PREPARE_SAMPLE_TRANSFER),
+                MMRobot.getInstance().mmSystems.scoringEndUnitRotator.setPosition(ScoringEndUnitRotator.ScoringRotatorState.SAMPLE_TRANSFER_POSE),
+
+                limelightGetter.getAlignToSample(hardwareMap).withTimeout(1500),
+                limelightGetter.getRotateToSample(),
+                limelightGetter.getOpenLinearToSample(),
+
+                new WaitCommand(200),
+                SampleIntake()
+        );
+    }
+
+    public static Command limeLightIntake_Auto(HardwareMap hardwareMap, PinpointDrive drive){
+        return new SequentialCommandGroup(
+                limelightGetter.getAlignToSampleAuto(hardwareMap, drive).withTimeout(1500),
+                limelightGetter.getRotateToSample(),
+                limelightGetter.getOpenLinearToSample()
         );
     }
 }
