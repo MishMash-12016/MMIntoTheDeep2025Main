a669d0b5a08aaf752f7b2287cc2fe06f3cc7821f
eran piestun <epiestun@gmail.com>
2025-04-09 17:32:36 +0300

    Vector Limelight ahh correction

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utils/MathTools.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utils/MathTools.java
@@ -9,7 +9,7 @@ public class MathTools {
         return Math.sqrt((p1.get(0) - p2.get(0)) * (p1.get(0) - p2.get(0)) + (p1.get(1) - p2.get(1)) * (p1.get(1) - p2.get(1)));
     }
 
-    public static double distance(double p1, double p2){
-        return Math.sqrt((p1 - p2) * (p1 - p2));
+    public static double distance(double x, double y){
+        return Math.sqrt(x* x + y * y);
     }
 }
