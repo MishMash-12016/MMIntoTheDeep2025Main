4f1c2fd1791ba750aef43641f13b5ba5fc6d62f1
drorlifshitz-12016 <mishmashbgu@gmail.com>
2025-04-13 17:50:57 +0300

    shahhwhhdhsadf sharab

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CommandGroup/limelight/strafeToSample.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CommandGroup/limelight/strafeToSample.java
@@ -25,12 +25,12 @@ import org.opencv.core.Mat;
 public class strafeToSample extends CommandBase {
     MecanumDrive.CancelableFollowTrajectoryAction strafeTrajectory;
 
-    public static double maxDistanceY = 408;
+    public static double maxDistanceY = 412;
     public static double maxDistanceYShort = 412;
-    public static double plusDistanceX = -0.1;
+    public static double plusDistanceX = 0.1;
     public static double accelerationMultiplierShort = 0.72;
     public static double limit = 10;
-    public static double theOtherSide = -1.5;
+    public static double theOtherSide = -2.5;
     public static double theOtherSideAdder = 1.5;
     public static double accelerationMultiplierLong = 1;
     Boolean finished = true;
